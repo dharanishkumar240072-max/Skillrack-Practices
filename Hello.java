b0466cb1ba489cdded01edd95792362487471631
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-02-22 10:06:08 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time = sc.next();
+        int hour = Integer.parseInt(time.substring(0, 2));
+        int minute = Integer.parseInt(time.substring(2, 4));
+        String period;
+        if(hour == 0) {
+            hour = 12;
+            period = "AM";
+        }
+        else if(hour == 12) {
+            period = "PM";
+        }
+        else if(hour > 12) {
+            hour = hour - 12;
+            period = "PM";
+        }
+        else {
+            period = "AM";
+        }
+        System.out.printf("%02d:%02d %s", hour, minute, period);
+    }
+}

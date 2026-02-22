9d8c1e715279429625d836b0a00c2682ed1bc7f1
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-02-22 10:30:08 +0530

    Update and rename Hello.java to Hour-format.java

--- Hour-format.java
+++ Hour-format.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Hello {
+public class Hour-format {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String time = sc.next();

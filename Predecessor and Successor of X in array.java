55894b55e44f10a99ddf028e0e18dfc82ac88096
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-02-28 09:54:17 +0530

    Create Predecessor and Successor of X in array.java

---
+++ Predecessor and Successor of X in array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		for (int i=0; i<N; i++){
+		    arr[i] = sc.nextInt();
+		}
+		int X = sc.nextInt();
+		System.out.print(arr[X-2]+" ");
+		System.out.print(arr[X-1]+ " ");
+		System.out.print(arr[X]);
+
+	}
+}

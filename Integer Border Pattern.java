3c606c1af16100d16e28233523d4a702800bb823
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-03-10 11:02:45 +0530

    Create Integer Border Pattern.java

---
+++ Integer Border Pattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 1;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || i == n || j == 1 || j == n){
+                    System.out.print(num + " ");
+                }
+                else{
+                    System.out.print("* ");
+                }
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}

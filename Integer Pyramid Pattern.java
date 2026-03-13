32e94f14ab30b855fdbf93f7565e3b18a29a7ba4
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-03-13 21:59:11 +0530

    Create Integer Pyramid Pattern.java

---
+++ Integer Pyramid Pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=1; i<=N; i++){
+		    for(int j=1; j<=N-i; j++){
+		        System.out.print("* ");
+		    }
+		    for(int j=1; j<=(i*2)-1; j++){
+		        System.out.print(j+" ");
+		    }
+		    System.out.print("\n");
+		}
+
+	}
+}

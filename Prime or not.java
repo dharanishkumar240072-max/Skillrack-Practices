1491ef487e9c16833d6ab8ebb949f1e043bd38a4
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-03-16 22:23:00 +0530

    Create Prime or not.java

---
+++ Prime or not.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n, i;
+        boolean isPrime = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime)
+            System.out.println(n + " is a Prime Number");
+        else
+            System.out.println(n + " is Not a Prime Number");
+    }
+}

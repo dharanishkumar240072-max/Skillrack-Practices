b408be3f9a56b098d478b6edbfd55bf79ff645f5
Dharanishkumar <dharanishkumar.240072@cse.ritchennai.edu.in>
2026-04-13 21:24:05 +0530

    Create Check if an Array is a Palindrome.java

---
+++ Check if an Array is a Palindrome.java
@@ -0,0 +1,22 @@
+public class PalindromeArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("Array is a Palindrome");
+        } else {
+            System.out.println("Array is not a Palindrome");
+        }
+    }
+}
